c6d526525897ce37bc425fd99b6f35c4dc7c2b23
Mj1234ba <bakriaweja@gmail.com>
2025-02-16 00:14:45 +0300

    fix login screen

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -11,14 +11,11 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
-import '../../features/auth/data/datasource/remote/remote_api_dataSource_impl.dart'
-    as _i631;
-import '../../features/auth/data/datasource/remote/remote_datasource_impl.dart'
-    as _i850;
-import '../../features/auth/data/repo/auth_repo_imp.dart' as _i39;
-import '../../features/auth/domain/repo/login_repo.dart' as _i543;
-import '../../features/auth/domain/usecase/loginUseCase.dart' as _i105;
-import '../../features/auth/presentation/cubit/cubit/login_cubit.dart' as _i481;
+import '../../features/login/data/datasource/remote/remote_api_dataSource_impl.dart'
+    as _i976;
+import '../../features/login/data/repo/auth_repo_imp.dart' as _i436;
+import '../../features/login/domain/usecase/loginUseCase.dart' as _i813;
+import '../../features/login/presentation/cubit/cubit/login_cubit.dart' as _i39;
 import '../api_manager/api_manager.dart' as _i266;
 
 extension GetItInjectableX on _i174.GetIt {
@@ -33,14 +30,14 @@ extension GetItInjectableX on _i174.GetIt {
       environmentFilter,
     );
     gh.singleton<_i266.ApiManager>(() => _i266.ApiManager());
-    gh.factory<_i850.RemoteDataSourceContract>(
-        () => _i631.RemoteApiDataSoucrceImpl(gh<_i266.ApiManager>()));
-    gh.factory<_i543.LoginRepo>(
-        () => _i39.LoginRepoImpl(gh<_i850.RemoteDataSourceContract>()));
-    gh.factory<_i105.Loginusecase>(
-        () => _i105.Loginusecase(gh<_i543.LoginRepo>()));
-    gh.factory<_i481.LoginCubit>(
-        () => _i481.LoginCubit(gh<_i105.Loginusecase>()));
+    gh.factory<_i436.LoginRepoImpl>(
+        () => _i436.LoginRepoImpl(gh<_i976.RemoteApiDataSoucrceImpl>()));
+    gh.factory<_i813.Loginusecase>(
+        () => _i813.Loginusecase(gh<_i436.LoginRepoImpl>()));
+    gh.factory<_i39.LoginCubit>(
+        () => _i39.LoginCubit(gh<_i813.Loginusecase>()));
+    gh.factory<_i976.RemoteApiDataSoucrceImpl>(
+        () => _i976.RemoteApiDataSoucrceImpl(gh<_i266.ApiManager>()));
     return this;
   }
 }
