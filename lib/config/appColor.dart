be17fe139b61870f1341d77667c8c7ae91647fbf
Mj1234ba <bakriaweja@gmail.com>
2025-02-20 15:41:36 +0300

    add file colorApp to manage the color of all screens app from one file

---
+++ lib/config/appColor.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Appcolor {
+  static Color grey = const Color(0xFF535353);
+  static Color white = const Color(0xFFF9F9F9);
+  static Color redError = const Color(0xFFCC1010);
+  static Color greenSuccess = const Color(0xFF11CE19);
+  static Color lightBlue = const Color(0xFFEDEFF3);
+  static Color lightGreen = const Color(0xFFCAF9CC);
+  static Color lightRed = const Color(0xFFF8D2D2);
+  static Color placeHolder = const Color(0xFFA6A6A6);
+  static Color blackBase = const Color(0xFF0F0F0F);
+  static Color black10 = const Color(0xFFCFCFCF);
+  static Color balck20 = const Color(0xFFAFAFAF);
+  static Color black30 = const Color(0xFF878787);
+  static Color black40 = const Color(0xFF5F5F5F);
+  static Color black50 = const Color(0xFF373737);
+  static Color black60 = const Color(0xFF0D0D0D);
+  static Color black70 = const Color(0xFF0A0A0A);
+  static Color black80 = const Color(0xFF080808);
+  static Color black90 = const Color(0xFF050505);
+  static Color black100 = const Color(0xFF030303);
+}
