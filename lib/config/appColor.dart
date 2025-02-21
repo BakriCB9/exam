624720bc40a208c4a9b1e43e5dcf2079525223b2
Mj1234ba <bakriaweja@gmail.com>
2025-02-21 15:49:01 +0300

    update on file appColor

--- lib/config/appColor.dart
+++ lib/config/appColor.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Appcolor {
+class AppColor {
   static Color grey = const Color(0xFF535353);
   static Color white = const Color(0xFFF9F9F9);
   static Color redError = const Color(0xFFCC1010);
@@ -9,6 +9,8 @@ class Appcolor {
   static Color lightGreen = const Color(0xFFCAF9CC);
   static Color lightRed = const Color(0xFFF8D2D2);
   static Color placeHolder = const Color(0xFFA6A6A6);
+
+  ///balck
   static Color blackBase = const Color(0xFF0F0F0F);
   static Color black10 = const Color(0xFFCFCFCF);
   static Color balck20 = const Color(0xFFAFAFAF);
@@ -20,4 +22,17 @@ class Appcolor {
   static Color black80 = const Color(0xFF080808);
   static Color black90 = const Color(0xFF050505);
   static Color black100 = const Color(0xFF030303);
+
+  ///blue
+  static Color blueBase = const Color(0xFF02369C);
+  static Color blue10 = const Color(0xFFCCD7EB);
+  static Color blue20 = const Color(0xFFABBCDE);
+  static Color blue30 = const Color(0xFF809ACD);
+  static Color blue40 = const Color(0xFF5679BD);
+  static Color blue50 = const Color(0xFF2C58AC);
+  static Color blue60 = const Color(0xFF022D82);
+  static Color blue70 = const Color(0xFF012468);
+  static Color blue80 = const Color(0xFF011B4E);
+  static Color blue90 = const Color(0xFF011234);
+  static Color blue100 = const Color(0xFF000B1F);
 }
