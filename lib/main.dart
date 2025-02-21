a67f10722a37612300cca5a7754f4c3a76ad8804
3mmrrrNabil <3mmrrr2004@gmail.com>
2025-02-20 16:39:57 -0800

    finish register screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {     
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
