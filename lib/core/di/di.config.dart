89f5738b1b28c5abe572acd293c07a66e9b850d2
Mj1234ba <bakriaweja@gmail.com>
2025-03-04 16:09:01 +0300

    feat(login): add some change on login feature

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -11,20 +11,22 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
-import '../../data/datasource/local_dataSource/local_dataSource_contract.dart'
+import '../../features/login/data/datasource/local_dataSource/local_dataSource_contract.dart'
     as _i968;
-import '../../data/datasource/local_dataSource/local_dataSource_impl.dart'
+import '../../features/login/data/datasource/local_dataSource/local_dataSource_impl.dart'
     as _i678;
-import '../../data/datasource/remote/remote_api_dataSource_impl.dart' as _i976;
-import '../../data/datasource/remote/remote_datasource_impl.dart' as _i512;
-import '../../data/datasource/remote_dataSource/remote_api_dataSource_impl.dart'
+import '../../features/login/data/datasource/remote/remote_api_dataSource_impl.dart'
+    as _i976;
+import '../../features/login/data/datasource/remote/remote_datasource_impl.dart'
+    as _i512;
+import '../../features/login/data/datasource/remote_dataSource/remote_api_dataSource_impl.dart'
     as _i503;
-import '../../data/datasource/remote_dataSource/remote_datasource_impl.dart'
+import '../../features/login/data/datasource/remote_dataSource/remote_datasource_impl.dart'
     as _i666;
-import '../../data/repo/auth_repo_imp.dart' as _i436;
-import '../../domain/repo/login_repo.dart' as _i0;
-import '../../domain/usecase/loginUseCase.dart' as _i813;
-import '../../presentation/cubit/cubit/login_cubit.dart' as _i39;
+import '../../features/login/data/repo/auth_repo_imp.dart' as _i436;
+import '../../features/login/domain/repo/login_repo.dart' as _i0;
+import '../../features/login/domain/usecase/loginUseCase.dart' as _i813;
+import '../../features/login/presentation/cubit/cubit/login_cubit.dart' as _i39;
 import '../api_manager/api_manager.dart' as _i266;
 import '../local_secure_storage/flutter_secure_storage.dart' as _i844;
 
