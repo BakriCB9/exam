6ea4cdc779c45b67960f332ec21b7dcf98e9049f
Mj1234ba <bakriaweja@gmail.com>
2025-03-09 18:13:53 +0300

    add api for get all subject in file constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,8 @@
 class AppConstants {
   static const String baseUrl = 'https://exam.elevateegy.com/';
   static const String login = 'api/v1/auth/signin';
+  static const String allSubject='api/v1/subjects';
+  // static const String 
 }
 
 class AppCashConstant {
