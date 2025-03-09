90ac8e0340c07c87d9314f14f7c78da49467e909
Mj1234ba <bakriaweja@gmail.com>
2025-03-09 18:19:31 +0300

    update on file api_manager(add param token in functionsk)

--- lib/core/api_manager/api_manager.dart
+++ lib/core/api_manager/api_manager.dart
@@ -1,16 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:exam_app/config/constants.dart';
 import 'package:injectable/injectable.dart';
+
 @singleton
 class ApiManager {
   Dio dio = Dio(BaseOptions(baseUrl: AppConstants.baseUrl));
-  Future<Response> get({required String endpoint, var requestbody}) async {
-    var response = await dio.get(AppConstants.login, data: requestbody);
+  Future<Response> get(
+      {required String endpoint, var requestbody, String? token}) async {
+    var response = await dio.get(AppConstants.login,
+        data: requestbody,
+        options: Options(headers: {"Authorization": "Bearer $token"}));
     return response;
   }
+
   Future<Response> post(
-     { String? token,required String endpoint, var data}) async {
-    var response = await dio.post(endpoint, data: data);
+      {String? token, required String endpoint, var data}) async {
+    var response = await dio.post(endpoint,
+        data: data,
+        options: Options(headers: {"Authorization": "Bearer $token"}));
     return response;
   }
 // Future<Response> get({required String ans,required String endpoint,}){
