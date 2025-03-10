ad3c71ea64997f251a4974c01e3ad71c25deb9bc
Mj1234ba <bakriaweja@gmail.com>
2025-03-10 22:18:13 +0300

    add routes in route file (examHomeRoute)

--- lib/core/routes_manager/route_generator.dart
+++ lib/core/routes_manager/route_generator.dart
@@ -1,28 +1,29 @@
- import 'package:exam_app/core/routes_manager/routes_name.dart';
+import 'package:exam_app/core/routes_manager/routes_name.dart';
+import 'package:exam_app/features/exam/presentation/view/home_exam_screen.dart';
 import 'package:exam_app/features/login/presentation/screens/login_screen.dart';
 import 'package:exam_app/features/register/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
-class RouteGenerator
- {
-static  Route<dynamic> getRoute(RouteSettings settings)
-{
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const SignUpPage());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case Routes.examHomeRoute:
+        return MaterialPageRoute(builder: (_) => HomeExamScreen());
+      default:
+        return unDefinedRoute();
+    }
+  }
 
-
-  switch(settings.name)
-  {
-    case Routes.registerRoute:
-      return MaterialPageRoute(builder: (_)=> const SignUpPage());
-    case Routes.loginRoute:
-      return MaterialPageRoute(builder: (_)=> const LoginScreen());
-    default:
-      return unDefinedRoute();
-
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text("No Route Found"),
+              ),
+            ));
   }
 }
-static  Route<dynamic> unDefinedRoute()
-{
-
-  return MaterialPageRoute(builder: (_)=> Scaffold(appBar: AppBar(title: const Text("No Route Found"),),));
-}
- }
