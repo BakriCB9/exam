f5eba15ca7a2dcb96b27c8c6110e671e243cc0f9
Mj1234ba <bakriaweja@gmail.com>
2025-03-10 22:23:59 +0300

    update on file exam_repo_impl

--- lib/features/exam/data/repo/exam_repo_impl.dart
+++ lib/features/exam/data/repo/exam_repo_impl.dart
@@ -1,5 +1,5 @@
+import 'package:dio/dio.dart';
 import 'package:exam_app/core/api_manager/api_result.dart';
-import 'package:exam_app/core/api_manager/excuter/api_excuter.dart';
 import 'package:exam_app/features/exam/data/datasource/remote_dataSource/remote_dataSource.dart';
 import 'package:exam_app/features/exam/data/model/all_subject/all_subject.dart';
 import 'package:exam_app/features/exam/domain/entity/subject_entity.dart/subject_entity.dart';
@@ -12,21 +12,14 @@ class ExamRepoImpl extends ExamRepo {
   ExamRepoImpl(this.remoteDatasourceExam);
   @override
   Future<ApiResult<List<AllSubjectEntity>>> getAllSubject() async {
-    ApiResult<AllSubject> ans = await excuteApi<AllSubject>(() {
-      return remoteDatasourceExam.getAllSubject();
-    });
-    switch (ans) {
-      case SuccessApiResult():
-        {
-          final list = ans.data?.subjects?.map((subjectElement) {
-            return subjectElement.toAllSubjectEntity();
-          }).toList();
-          return SuccessApiResult<List<AllSubjectEntity>>(list);
-        }
-      case ErrorApiResult():
-        {
-          return ErrorApiResult<List<AllSubjectEntity>>(ans.exception);
-        }
+    try {
+      AllSubject ans = await remoteDatasourceExam.getAllSubject();
+      final list = ans.subjects?.map((subjectElement) {
+        return subjectElement.toAllSubjectEntity();
+      }).toList();
+      return SuccessApiResult<List<AllSubjectEntity>>(list);
+    } on DioException catch (exception) {
+      return ErrorApiResult<List<AllSubjectEntity>>(exception);
     }
   }
 }
